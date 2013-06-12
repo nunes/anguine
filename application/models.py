deb30e6f8ecda7fa541cd97e1b5f258a281e046c
nunes <nunes@es-robot.com>
2013-06-12 14:29:05 +0200

    added OwnerAuthorizer on rest services

--- application/models.py
+++ application/models.py
@@ -4,5 +4,8 @@ class Task(db.Model):
     """
     Task model
     """
-    description = db.StringProperty()
+    description = db.StringProperty(required=True)
     label = db.StringProperty()
+    owner = db.UserProperty()
+    created = db.DateTimeProperty(auto_now_add=True)
+    edited = db.DateTimeProperty(auto_now=True)
--- rest_handler.py
+++ rest_handler.py
@@ -4,10 +4,48 @@ import sys
 root = os.path.split(__file__)[0]
 sys.path.insert(0, os.path.join(root, 'lib'))
 
-
-from application import models
 import rest
+from application import models
+from google.appengine.api import users
 from google.appengine.ext import webapp
+
+
+class OwnerAuthorizer(rest.Authorizer):
+
+    def can_read(self, dispatcher, model):
+        if(model.owner != users.get_current_user()):
+            dispatcher.not_found()
+
+    def filter_read(self, dispatcher, models):
+        return self.filter_models(models)
+
+    def check_query(self, dispatcher, query_expr, query_params):
+        query_params.append(users.get_current_user())
+        if(not query_expr):
+            query_expr = 'WHERE owner = :%d' % (len(query_params))
+        else:
+            query_expr += ' AND owner = :%d' % (len(query_params))
+        return query_expr
+
+    def can_write(self, dispatcher, model, is_replace):
+        if(not model.is_saved()):
+            # creating a new model
+            model.owner = users.get_current_user()
+        elif(model.owner != users.get_current_user()):
+            dispatcher.not_found()
+
+    def filter_write(self, dispatcher, models, is_replace):
+        return self.filter_models(models)
+
+    def can_delete(self, dispatcher, model_type, model_key):
+        query = model_type.all().filter("owner = ", users.get_current_user()).filter("__key__ = ", model_key)
+        if(len(query.fetch(1)) == 0):
+            dispatcher.not_found()
+
+    def filter_models(self, models):
+        cur_user = users.get_current_user()
+        models[:] = [model for model in models if model.owner == cur_user]
+        return models
 
 
 application = webapp.WSGIApplication([('/rest/.*', rest.Dispatcher)])
@@ -15,3 +53,5 @@ application = webapp.WSGIApplication([('/rest/.*', rest.Dispatcher)])
 rest.Dispatcher.base_url = "/rest"
 
 rest.Dispatcher.add_models_from_module(models)
+
+rest.Dispatcher.authorizer = OwnerAuthorizer()
